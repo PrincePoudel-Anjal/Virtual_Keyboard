48d87dc4feb01a35f683ffa29bacf7be73cb57d3
PrincePoudel-Anjal <prince12poudel@gmail.com>
2025-10-14 16:36:38 +0545

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import sys
 import mediapipe as mp
 import math
 
@@ -19,6 +18,14 @@ def find_distance(image,p1,p2):
 
     return length, image
 
+def box_around_hands(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2)
+    hsvcimage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lowerlimit = np.array([22, 100, 100])
+    upperlimit = np.array([30, 255, 255])
+    mask = cv2.inRange(hsvcimage, lowerlimit, upperlimit)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 
 # Class of Buttons
 class Button:
@@ -35,6 +42,7 @@ class Button:
 def hand_detection(image,Draw,mphands,hands,draw):
     positionx = [0] * 20
     positiony = [0] * 20
+    Area = 0
 
 
     rgbimage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -49,12 +57,18 @@ def hand_detection(image,Draw,mphands,hands,draw):
                         positionx[index], positiony[index] = int(landmark.x * w), int(landmark.y * h)
                     if index == 4:
                         positionx[4], positiony[4] = int(landmark.x * w), int(landmark.y * h)
+                    if index == 0:
+                        positionx[0], positiony[0] = int(landmark.x * w), int(landmark.y * h)
+                    if index == 17:
+                        positionx17, positiony[17] = int(landmark.x * w), int(landmark.y * h)
 
-            draw.draw_landmarks(image, hand, mphands.HAND_CONNECTIONS)
-    create_buttons(image,positionx,positiony)
+                    draw.draw_landmarks(image, hand, mphands.HAND_CONNECTIONS)
+    area = (positionx[0] - positionx[4]) * (positiony[0] - positiony[8])
+    # print(area)
+    create_buttons(image,positionx,positiony,area)
 
 # Creating the button
-def create_buttons(image,px,py):
+def create_buttons(image,px,py,area):
     global text
     color = (255, 255,255)
     keys = [
@@ -66,7 +80,9 @@ def create_buttons(image,px,py):
     y = 100
     button = [0] * 30
     count = 0
-    cv2.rectangle(image, (150, 400), (500 + 200, 500), (128, 128, 128), -1) #Writing Box
+    cv2.rectangle(image, (150, 500), (500 + 200, 600), (128, 128, 128), -1) #Writing Box
+
+
     for row, i in enumerate(keys):
         x = 150
         y = y + 55
@@ -79,13 +95,17 @@ def create_buttons(image,px,py):
                 length,image = find_distance(image,(px[8],py[8]),(px[4],py[4]))
 
                 print(length)
-                if 24<length<26:
-                    text = text + j
+
+                if 40000 <area <60000:
+                    cv2.rectangle(image, (px[4], py[8]), (px[0], py[0]), (0, 255, 0), 2)
+                    if 50<length<53:
+                        text = text + j
+
 
             else:
                 button[count].draw_rectangle(image, color)
             x += 50 + 5
-            cv2.putText(image, text, (150, 470), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=7)
+            cv2.putText(image, text, (150, 570), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=7)
             count += 1
 
 # main
